f5bfd5b8a980a567897a071e763da7dbdc28acfb
MarceloCodeCraft <DevMarceloJesus@hotmail.com>
2024-03-06 22:54:42 -0300

    Commit inicial

---
+++ print.py
@@ -0,0 +1,17 @@
+numero1 = float(input("Digite o primeiro número: "))
+
+numero2 = float(input("Digite o segundo número: "))
+
+operador = input("Digite o operador (+, -, *, /): ")
+
+resultado = eval(f"{numero1} {operador} {numero2}")
+
+
+print(f"O resultado é: {resultado}")
+
+
+
+
+
+     
+
